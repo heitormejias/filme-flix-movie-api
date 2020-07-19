8259e3b781389085161c65a2514985cd265a351b
Claudio B. Moreno <klauddius@gmail.com>
2020-07-18 21:55:40 -0300

    Initial commit

---
+++ src/main/java/br/com/klauddius/filmeflix/movie/FilmeFlixMovieApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.klauddius.filmeflix.movie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilmeFlixMovieApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilmeFlixMovieApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/klauddius/filmeflix/movie/FilmeFlixMovieApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.klauddius.filmeflix.movie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilmeFlixMovieApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
